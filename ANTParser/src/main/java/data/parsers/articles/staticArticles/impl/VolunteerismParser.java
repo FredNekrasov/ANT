d26e37b08d5e3803933d7762ae6f1e174ef69440
FredNekrasov <frednekrasov49@gmail.com>
2024-09-16 07:30:24 +0300

    creating a parser class to extract data from a volunteerism page

---
+++ ANTParser/src/main/java/data/parsers/articles/staticArticles/impl/VolunteerismParser.java
@@ -0,0 +1,49 @@
+package data.parsers.articles.staticArticles.impl;
+
+import jakarta.inject.Inject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class VolunteerismParser {
+    @Inject public VolunteerismParser() {}
+    private Document parseData() {
+        try {
+            return Jsoup.connect("https://hramalnevskogo.ru/page45347187.html").get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private String[] parseText() {
+        var data = parseData().getElementsByClass("t-container")
+                .stream()
+                .map(it -> it.children().html())
+                .limit(1)
+                .collect(Collectors.joining());
+        return Arrays.stream(data.split("^[<\\w\\s=\"-:>;]*"))
+                .map(String::trim)
+                .collect(Collectors.joining("\n"))
+                .split("<[\\w>\\s=\"-:;]*");
+    }
+    public String parseTitle() {
+        return Arrays.stream(parseText()).filter(it -> !it.isEmpty()).toList().getFirst().trim();
+    }
+    public List<String> parseContent() {
+        return parseData().getElementsByClass("t156__wrapper")
+                .stream()
+                .map(it -> it.getElementsByClass("t156__item"))
+                .flatMap(Collection::stream)
+                .map(it -> it.children().attr("data-original"))
+                .toList();
+    }
+    public String parseDescription() {
+        return Arrays.stream(parseText())
+                .map(String::trim)
+                .skip(1)
+                .filter(it -> !it.isBlank())
+                .collect(Collectors.joining("\n"));
+    }
+}
