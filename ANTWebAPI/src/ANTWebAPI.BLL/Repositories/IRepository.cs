bbfdf4e4bbf79e4c25bcecde4ed41d587ebf14c6
FredNekrasov <frednekrasov49@gmail.com>
2024-09-18 22:34:18 +0300

    creating repository interfaces. one of them is needed to interact with the database, and the other is needed to transfer data to the client

---
+++ ANTWebAPI/src/ANTWebAPI.BLL/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace ANTWebAPI.BLL.Repositories;
+
+public interface IRepository<M>
+{
+    Task InsertAsync(M model);
+
+    Task<bool> DeleteAsync(long id);
+
+    Task<List<M>> GetListAsync();
+
+    Task<M?> GetModelAsync(long id);
+
+    Task<bool> UpdateAsync(M model);
+
+    Task<bool> IsEntityExistsAsync(long id);
+}
