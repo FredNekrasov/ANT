bbfc12eae02b8fc1e962c0dfcc92e3cd52301ce4
FredNekrasov <frednekrasov49@gmail.com>
2024-09-19 17:45:07 +0300

    creating an ANTDbContext to interact with the database

---
+++ ANTWebAPI/src/ANTWebAPI.DAL/Database/ANTDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ANTWebAPI.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ANTWebAPI.DAL.Database;
+
+public class ANTDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ANT;Integrated Security=true;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    public DbSet<Catalog> Catalogs { get; set; }
+    public DbSet<Article> Articles { get; set; }
+    public DbSet<Content> Contents { get; set; }
+}
