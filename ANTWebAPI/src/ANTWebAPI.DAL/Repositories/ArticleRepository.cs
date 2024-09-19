d9602b5e5482a1a1cd7962922cc050dbe70e7a42
FredNekrasov <frednekrasov49@gmail.com>
2024-09-19 17:47:39 +0300

    creating repositories for working with data in a database

---
+++ ANTWebAPI/src/ANTWebAPI.DAL/Repositories/ArticleRepository.cs
@@ -0,0 +1,65 @@
+﻿using ANTWebAPI.BLL.Models;
+using ANTWebAPI.BLL.Repositories;
+using ANTWebAPI.DAL.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.DAL.Repositories;
+
+internal class ArticleRepository(ANTDbContext context) : IRepository<Article>
+{
+    private readonly ANTDbContext _context = context;
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        if (await _context.Contents.AnyAsync(e => e.ArticleId == id)) return false;
+        var model = await _context.Articles.FirstAsync(e => e.Id == id);
+        _context.Articles.Remove(model);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<List<Article>> GetListAsync()
+    {
+        List<Article> articleModels = [];
+        var articles = await _context.Articles.ToListAsync();
+        foreach (var article in articles)
+        {
+            var catalog = await _context.Catalogs.FirstOrDefaultAsync(e => e.Id == article.CatalogId);
+            if (catalog == null) continue;
+            var articleModel = new Article
+            {
+                Id = article.Id,
+                Catalog = catalog,
+                Title = article.Title,
+                Description = article.Description,
+                DateOrBanner = article.DateOrBanner
+            };
+            articleModels.Add(articleModel);
+        }
+        return articleModels;
+    }
+
+    public async Task<Article?> GetModelAsync(long id) => await _context.Articles.FirstOrDefaultAsync(e => e.Id == id);
+
+    public async Task InsertAsync(Article model)
+    {
+        await _context.Articles.AddAsync(model);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> IsEntityExistsAsync(long id) => await _context.Articles.AnyAsync(e => e.Id == id);
+
+    public async Task<bool> UpdateAsync(Article model)
+    {
+        _context.Entry(model).State = EntityState.Modified;
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ ANTWebAPI/src/ANTWebAPI.DAL/Repositories/ChapterRepository.cs
@@ -0,0 +1,34 @@
+﻿using ANTWebAPI.BLL.Models;
+using ANTWebAPI.BLL.Repositories;
+using ANTWebAPI.DAL.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.DAL.Repositories;
+
+internal class ChapterRepository(ANTDbContext context) : IChapterRepository
+{
+    private readonly ANTDbContext _context = context;
+
+    public async Task<List<Chapter>> GetListAsync()
+    {
+        var contentList = await _context.Contents.ToListAsync();
+        var articleList = await _context.Articles.ToListAsync();
+        var chapterList = new List<Chapter>();
+        foreach (var article in articleList)
+        {
+            var content = contentList.Where(content => content.ArticleId == article.Id).Select(content => content.Data).ToList();
+            var catalog = await _context.Catalogs.FindAsync(article.CatalogId);
+            var chapter = new Chapter
+            {
+                Id = article.Id,
+                Catalog = catalog ?? article.Catalog,
+                Title = article.Title,
+                Description = article.Description,
+                DateOrBanner = article.DateOrBanner,
+                Content = content
+            };
+            chapterList.Add(chapter);
+        }
+        return chapterList;
+    }
+}
