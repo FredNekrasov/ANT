32a4e92c1909b809e3ccd7534a9bca4e44c2a735
FredNekrasov <frednekrasov49@gmail.com>
2024-09-19 17:42:35 +0300

    creating configuration classes for entities

---
+++ ANTWebAPI/src/ANTWebAPI.DAL/Database/Configurations/CatalogConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using ANTWebAPI.BLL.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.DAL.Database.Configurations;
+
+internal class CatalogConfiguration : IEntityTypeConfiguration<Catalog>
+{
+    public void Configure(EntityTypeBuilder<Catalog> entity)
+    {
+        entity.ToTable("Catalogs").HasKey(t => t.Id);
+        entity.HasMany(i => i.Articles).WithOne(i => i.Catalog).HasForeignKey(i => i.CatalogId);
+    }
+}
---
+++ ANTWebAPI/src/ANTWebAPI.DAL/Database/Configurations/ContentConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using ANTWebAPI.BLL.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.DAL.Database.Configurations;
+
+internal class ContentConfiguration : IEntityTypeConfiguration<Content>
+{
+    public void Configure(EntityTypeBuilder<Content> entity)
+    {
+        entity.ToTable("Contents").HasKey(t => t.Id);
+        entity.Property(t => t.ArticleId).HasColumnName("article_id");
+    }
+}
