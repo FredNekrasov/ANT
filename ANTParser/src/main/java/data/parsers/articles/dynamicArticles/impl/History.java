0ea7b792c9ef159df205ba258c7f7863ef33c9ba
FredNekrasov <frednekrasov49@gmail.com>
2024-09-20 10:27:33 +0300

    creating a parser class to extract data from a history page

---
+++ ANTParser/src/main/java/data/parsers/articles/dynamicArticles/impl/History.java
@@ -0,0 +1,11 @@
+package data.parsers.articles.dynamicArticles.impl;
+
+import data.parsers.articles.dynamicArticles.DATParser;
+import jakarta.inject.Inject;
+
+public class History extends DATParser {
+    @Inject public History() {}
+    public String parseDescription(String url) {
+        return super.parseDescription(url, "t-redactor__text");
+    }
+}
