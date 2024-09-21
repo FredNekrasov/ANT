3694040606f6b632ab4a654e59a711d3d4e8074e
FredNekrasov <frednekrasov49@gmail.com>
2024-09-21 09:29:22 +0300

    creating mappers that convert entity to dto and vice versa

---
+++ ANTWebAPI/src/ANTWebAPI.PL/Mappers/ChapterMapper.cs
@@ -0,0 +1,17 @@
+﻿using ANTWebAPI.BLL.Models;
+using ANTWebAPI.PL.DTOs;
+
+namespace ANTWebAPI.PL.mappers;
+
+internal static class ChapterMapper
+{
+    internal static ChapterDTO ToDto(this Chapter chapter) => new()
+    {
+        Id = chapter.Id,
+        Catalog = chapter.Catalog.ToDto(),
+        Title = chapter.Title,
+        Description = chapter.Description,
+        DateOrBanner = chapter.DateOrBanner,
+        Content = chapter.Content
+    };
+}
---
+++ ANTWebAPI/src/ANTWebAPI.PL/Mappers/ContentMapper.cs
@@ -0,0 +1,21 @@
+﻿using ANTWebAPI.BLL.Models;
+using ANTWebAPI.PL.DTOs;
+
+namespace ANTWebAPI.PL.mappers;
+
+internal static class ContentMapper
+{
+    internal static ContentDTO ToDto(this Content content) => new()
+    {
+        Id = content.Id,
+        ArticleId = content.ArticleId,
+        Data = content.Data
+    };
+
+    internal static Content ToModel(this ContentDTO content) => new()
+    {
+        Id = content.Id,
+        ArticleId = content.ArticleId,
+        Data = content.Data
+    };
+}
