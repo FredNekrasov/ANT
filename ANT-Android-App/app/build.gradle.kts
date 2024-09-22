45895e6b72ad7defdf53ad895f2c00e8d0c419ea
FredNekrasov <frednekrasov49@gmail.com>
2024-09-22 22:19:29 +0300

    adding plugins and libraries to the app module

--- ANT-Android-App/app/build.gradle.kts
+++ ANT-Android-App/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.sqldelight)
 }
 
 android {
@@ -48,20 +50,19 @@ composeCompiler {
     metricsDestination = layout.buildDirectory.dir("composeMetrics")
 }
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.bundles.koin)
+    implementation(libs.bundles.navigation)
+    // data
+    implementation(libs.bundles.network)
+    implementation(libs.bundles.database)
+    // presentation
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.bundles.app)
+    implementation(libs.bundles.presentation)
+    // test
+    testImplementation(libs.bundles.test)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.bundles.android.test)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
