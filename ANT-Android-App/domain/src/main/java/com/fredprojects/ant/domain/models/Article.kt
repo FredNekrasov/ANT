dc4a00864e16f9e3570a208d93171b688638928f
FredNekrasov <frednekrasov49@gmail.com>
2024-09-25 20:01:11 +0300

    creating a model

---
+++ ANT-Android-App/domain/src/main/java/com/fredprojects/ant/domain/models/Article.kt
@@ -0,0 +1,19 @@
+package com.fredprojects.ant.domain.models
+
+/**
+ * Data class for Article model used in domain layer of the application
+ *  @param title article title
+ *  @param description article description
+ *  @param date date or banner of the article
+ *  @param articleType type of the article
+ *  @param content is in the form of a list of lines with links to images or contact information
+ *  @param id id of the article
+ */
+data class Article(
+    val title: String,
+    val description: String,
+    val date: String,
+    val articleType: String,
+    val content: List<String>,
+    val id: Int = 0
+)
