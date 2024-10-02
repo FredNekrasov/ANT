13f79f7b2dd5c445252e2043747446b8484a40bc
FredNekrasov <frednekrasov49@gmail.com>
2024-10-02 21:23:49 +0300

    creating an ArticleVM

---
+++ ANT-Android-App/presentation/src/main/java/com/fredprojects/ant/presentation/screens/viewModels/ArticleVM.kt
@@ -0,0 +1,27 @@
+package com.fredprojects.ant.presentation.screens.viewModels
+
+import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fredprojects.ant.domain.repository.IArticleRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class ArticleVM(
+    private val getArticlesRepo: IArticleRepository,
+    private val context: Application
+) : ViewModel() {
+    private val articlesMSF = MutableStateFlow(ArticleState())
+    val articlesSF = articlesMSF.asStateFlow()
+    init {
+        viewModelScope.launch {
+            getArticlesRepo.getList().flowOn(Dispatchers.IO).collectLatest {
+                articlesMSF.emit(articlesSF.value.copy(list = it.list, status = it.status))
+            }
+        }
+    }
+    fun openApp(url: String) = context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+}
