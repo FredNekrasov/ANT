05333efbe7ee81305701398259206825a3a5f332
FredNekrasov <frednekrasov49@gmail.com>
2024-10-11 17:18:47 +0300

    creating an ANTWebAPI.Tests.DALTests.ArticleRepo.UpdateArticleUT class to test the UpdateAsync method of the ArticleRepository class

---
+++ ANTWebAPI/src/ANTWebAPI.Tests/DALTests/ArticleRepo/UpdateArticleUT.cs
@@ -0,0 +1,30 @@
+﻿using ANTWebAPI.DAL.Database;
+using ANTWebAPI.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.Tests.DALTests.ArticleRepo;
+
+public class UpdateArticleUT
+{
+    [Fact]
+    public async Task SuccessUpdateArticleAsync()
+    {
+        using ANTDbContext context = new();
+        var repository = new ArticleRepository(context);
+        var article = await context.Articles.OrderBy(e => e.Id).LastOrDefaultAsync();
+        Assert.NotNull(article);
+        article.Title = "Updated Article";
+        await repository.UpdateAsync(article);
+        var updatedArticle = await repository.GetModelAsync(article.Id);
+        Assert.NotNull(updatedArticle);
+        Assert.Equal(article.Title, updatedArticle.Title);
+    }
+    [Fact]
+    public async Task FailedUpdateArticleAsync()
+    {
+        using ANTDbContext context = new();
+        var repository = new ArticleRepository(context);
+        var article = new BLL.Models.Article() { Id = -1, Title = "TestUpdateArticle", CatalogId = 1, Description = "TestUpdateArticle", DateOrBanner = DateTime.Today.ToString() };
+        Assert.False(await repository.UpdateAsync(article));
+    }
+}
