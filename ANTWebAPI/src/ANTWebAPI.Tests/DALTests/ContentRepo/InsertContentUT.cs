eabfe56f441b25075c043672f0684749459d0a84
FredNekrasov <frednekrasov49@gmail.com>
2024-10-12 17:03:47 +0300

    creating an ANTWebAPI.Tests.DALTests.ContentRepo.InsertContentUT class to test the InsertAsync method of the ContentRepository class

---
+++ ANTWebAPI/src/ANTWebAPI.Tests/DALTests/ContentRepo/InsertContentUT.cs
@@ -0,0 +1,40 @@
+﻿using ANTWebAPI.BLL.Models;
+using ANTWebAPI.DAL.Database;
+using ANTWebAPI.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.Tests.DALTests.ContentRepo;
+
+public class InsertContentUT
+{
+    [Fact]
+    public async Task SuccessInsertContentAsync()
+    {
+        // need to specify an existing articleId
+        var content = new Content() { ArticleId = 2, Data = "Test" };
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        await repository.InsertAsync(content);
+        var insertedContent = await repository.GetModelAsync(content.Id);
+        Assert.NotNull(insertedContent);
+        Assert.Equal(content.Data, insertedContent.Data);
+    }
+    [Fact]
+    public async Task InsertEmptyContentAsync()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        await Assert.ThrowsAsync<DbUpdateException>(() => repository.InsertAsync(new Content()));
+        var insertedContent = await context.Contents.FirstOrDefaultAsync(e => e.Data == string.Empty);
+        Assert.Null(insertedContent);
+    }
+    [Fact]
+    public async Task FailedInsertContentAsync()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        await Assert.ThrowsAsync<DbUpdateException>(() => repository.InsertAsync(new Content() { Id = 1, ArticleId = 1, Data = "InsertContentTest" }));
+        var insertedContent = await context.Contents.FirstOrDefaultAsync(e => e.Data == "InsertContentTest");
+        Assert.Null(insertedContent);
+    }
+}
