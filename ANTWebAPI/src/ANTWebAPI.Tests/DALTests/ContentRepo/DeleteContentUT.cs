a845d6f09ce1ef0c7c1afe833d0f4e44ca78e6c5
FredNekrasov <frednekrasov49@gmail.com>
2024-10-12 17:09:36 +0300

    creating an ANTWebAPI.Tests.DALTests.ContentRepo.DeleteContentUT class to test the DeleteAsync method of the ContentRepository class

---
+++ ANTWebAPI/src/ANTWebAPI.Tests/DALTests/ContentRepo/DeleteContentUT.cs
@@ -0,0 +1,27 @@
+﻿using ANTWebAPI.DAL.Database;
+using ANTWebAPI.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.Tests.DALTests.ContentRepo;
+
+public class DeleteContentUT
+{
+    [Fact]
+    public async Task SuccessDeleteContent()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        var content = await context.Contents.OrderBy(e => e.Id).LastOrDefaultAsync();
+        Assert.NotNull(content);
+        Assert.True(await repository.DeleteAsync(content.Id));
+        var deletedContent = await repository.GetModelAsync(content.Id);
+        Assert.Null(deletedContent);
+    }
+    [Fact]
+    public async Task FailedDeleteContent()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        Assert.False(await repository.DeleteAsync(78L));// trying to delete a content with non existent id
+    }
+}
