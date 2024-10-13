7809980fce12c339f2c51d621fa90d9611a5ef85
FredNekrasov <frednekrasov49@gmail.com>
2024-10-13 09:49:29 +0300

    creating unit tests for converters. (CatalogMapperUT.cs, ArticleMapperUT.cs, ContentMapperUT.cs, ChapterMapperUT.cs)

---
+++ ANTWebAPI/src/ANTWebAPI.Tests/PLTests/Mappers/CatalogMapperUT.cs
@@ -0,0 +1,21 @@
+﻿using ANTWebAPI.BLL.Models;
+using ANTWebAPI.PL.DTOs;
+using ANTWebAPI.PL.mappers;
+
+namespace ANTWebAPI.Tests.PLTests.Mappers;
+
+public class CatalogMapperUT
+{
+    [Fact]
+    public void MapToCatalog()
+    {
+        var catalogDto = new CatalogDTO() { Id = 1, Name = "Catalog" };
+        Assert.NotNull(catalogDto.ToModel());
+    }
+    [Fact]
+    public void MapToCatalogDTO()
+    {
+        var catalog = new Catalog() { Id = 1, Name = "Catalog" };
+        Assert.NotNull(catalog.ToDto());
+    }
+}
---
+++ ANTWebAPI/src/ANTWebAPI.Tests/PLTests/Mappers/ContentMapperUT.cs
@@ -0,0 +1,21 @@
+﻿using ANTWebAPI.BLL.Models;
+using ANTWebAPI.PL.DTOs;
+using ANTWebAPI.PL.mappers;
+
+namespace ANTWebAPI.Tests.PLTests.Mappers;
+
+public class ContentMapperUT
+{
+    [Fact]
+    public void MapToContent()
+    {
+        var contentDto = new ContentDTO() { Id = 1, ArticleId = 1, Data = "Content" };
+        Assert.NotNull(contentDto.ToModel());
+    }
+    [Fact]
+    public void MapToContentDTO()
+    {
+        var content = new Content() { Id = 1, ArticleId = 1, Data = "Content" };
+        Assert.NotNull(content.ToDto());
+    }
+}
