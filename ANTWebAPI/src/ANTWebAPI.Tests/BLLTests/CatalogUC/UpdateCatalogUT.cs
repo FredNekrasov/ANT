5115cc4d3d7a4aba9aba5d86710c70676c67d978
FredNekrasov <frednekrasov49@gmail.com>
2024-10-14 23:31:19 +0300

    creating an ANTWebAPI.Tests.BLLTests.CatalogUC.UpdateCatalogUT class to test the UpdateAsync method of the `CatalogUseCases` class

---
+++ ANTWebAPI/src/ANTWebAPI.Tests/BLLTests/CatalogUC/UpdateCatalogUT.cs
@@ -0,0 +1,36 @@
+﻿using ANTWebAPI.BLL.UseCases;
+using ANTWebAPI.DAL.Database;
+using ANTWebAPI.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.Tests.BLLTests.CatalogUC;
+
+public class UpdateCatalogUT
+{
+    [Fact]
+    public async Task SuccessUpdateCatalogAsync()
+    {
+        using ANTDbContext context = new();
+        var repository = new CatalogRepository(context);
+        var useCases = new CatalogUseCases(repository);
+        var catalog = await context.Catalogs.OrderBy(e => e.Id).LastOrDefaultAsync();
+        Assert.NotNull(catalog);
+        catalog.Name = "Updated Catalog";
+        Assert.True(await useCases.UpdateAsync(catalog.Id, catalog));
+        var updatedCatalog = await useCases.GetAsync(catalog.Id);
+        Assert.NotNull(updatedCatalog);
+        Assert.Equal(catalog.Name, updatedCatalog.Name);
+    }
+    [Fact]
+    public async Task FailedUpdateCatalogAsync()
+    {
+        using ANTDbContext context = new();
+        var repository = new CatalogRepository(context);
+        var useCases = new CatalogUseCases(repository);
+        var catalog = new BLL.Models.Catalog() { Id = -1, Name = "TestUpdateCatalog(BLL)" };
+        Assert.False(await useCases.UpdateAsync(778899L, catalog));
+        Assert.False(await useCases.UpdateAsync(catalog.Id, catalog));
+        catalog.Id = 778899L;
+        Assert.Null(await useCases.UpdateAsync(catalog.Id, catalog));
+    }
+}
