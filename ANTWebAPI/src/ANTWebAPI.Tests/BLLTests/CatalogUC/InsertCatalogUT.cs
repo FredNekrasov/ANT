065b80c14230f058edb591d77d6c8feba960db09
FredNekrasov <frednekrasov49@gmail.com>
2024-10-14 23:29:28 +0300

    creating an ANTWebAPI.Tests.BLLTests.CatalogUC.InsertCatalogUT class to test the InsertAsync method of the `CatalogUseCases` class

---
+++ ANTWebAPI/src/ANTWebAPI.Tests/BLLTests/CatalogUC/InsertCatalogUT.cs
@@ -0,0 +1,48 @@
+﻿using ANTWebAPI.BLL.Models;
+using ANTWebAPI.BLL.UseCases;
+using ANTWebAPI.BLL.Utils;
+using ANTWebAPI.DAL.Database;
+using ANTWebAPI.DAL.Repositories;
+
+namespace ANTWebAPI.Tests.BLLTests.CatalogUC;
+
+public class InsertCatalogUT
+{
+    [Fact]
+    public async Task SuccessInsertCatalogAsync()
+    {
+        var catalog = new Catalog() { Name = "BLLTest" };
+        using ANTDbContext context = new();
+        var repository = new CatalogRepository(context);
+        var useCases = new CatalogUseCases(repository);
+        Assert.True(await useCases.InsertAsync(catalog));
+    }
+    [Fact]
+    public async Task InsertEmptyCatalogAsync()
+    {
+        using ANTDbContext context = new();
+        var repository = new CatalogRepository(context);
+        var useCases = new CatalogUseCases(repository);
+        Assert.False(await useCases.InsertAsync(new Catalog()));
+    }
+    [Fact]
+    public async Task FailedInsertCatalogAsync()
+    {
+        using ANTDbContext context = new();
+        var repository = new CatalogRepository(context);
+        var useCases = new CatalogUseCases(repository);
+        Assert.False(await useCases.InsertAsync(new Catalog() { Id = 1, Name = "InsertCatalogTest(BLL)" }));
+    }
+    [Fact]
+    public void SuccessIsCatalogValid()
+    {
+        var catalog = new Catalog() { Name = "InsertCatalogTest(BLL)" };
+        Assert.True(catalog.IsDataValid());
+    }
+    [Fact]
+    public void FailedIsCatalogValid()
+    {
+        var catalog = new Catalog();
+        Assert.False(catalog.IsDataValid());
+    }
+}
