c70425af6e7838fac89eb66a6eb14f31754a1d7a
FredNekrasov <frednekrasov49@gmail.com>
2024-10-16 13:56:47 +0300

    creating an ANTWebAPI.Tests.BLLTests.ContentUC.DeleteContentUT class to test the DeleteAsync method of the `ContentUseCases` class

---
+++ ANTWebAPI/src/ANTWebAPI.Tests/BLLTests/ContentUC/DeleteContentUT.cs
@@ -0,0 +1,30 @@
+﻿using ANTWebAPI.BLL.UseCases;
+using ANTWebAPI.DAL.Database;
+using ANTWebAPI.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.Tests.BLLTests.ContentUC;
+
+public class DeleteContentUT
+{
+    [Fact]
+    public async Task SuccessDeleteContent()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        var useCases = new ContentUseCases(repository);
+        var content = await context.Contents.OrderBy(e => e.Id).LastOrDefaultAsync();
+        Assert.NotNull(content);
+        Assert.True(await useCases.DeleteAsync(content.Id));
+        var deletedContent = await useCases.GetAsync(content.Id);
+        Assert.Null(deletedContent);
+    }
+    [Fact]
+    public async Task FailedDeleteContent()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        var useCases = new ContentUseCases(repository);
+        Assert.False(await repository.DeleteAsync(7788L));// trying to delete a content with non existent id
+    }
+}
