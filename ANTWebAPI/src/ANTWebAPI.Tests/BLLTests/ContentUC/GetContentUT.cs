01e19f5c16bb27ed7563504cd4321b4ebc51c5cb
FredNekrasov <frednekrasov49@gmail.com>
2024-10-16 13:58:10 +0300

    creating an ANTWebAPI.Tests.BLLTests.ContentUC.GetContentUT class to test GetAsync and GetListAsync methods of the `ContentUseCases` class

---
+++ ANTWebAPI/src/ANTWebAPI.Tests/BLLTests/ContentUC/GetContentUT.cs
@@ -0,0 +1,42 @@
+﻿using ANTWebAPI.BLL.UseCases;
+using ANTWebAPI.DAL.Database;
+using ANTWebAPI.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.Tests.BLLTests.ContentUC;
+
+public class GetContentUT
+{
+    [Fact]
+    public async Task SuccessGetContent()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        var useCases = new ContentUseCases(repository);
+        var anyContent = await context.Contents.FirstOrDefaultAsync();
+        Assert.NotNull(anyContent);
+        var foundContent = await useCases.GetAsync(anyContent.Id);
+        Assert.NotNull(foundContent);
+        Assert.Equal(anyContent.Data, foundContent.Data);
+    }
+    [Fact]
+    public async Task FailedGetContent()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        var useCases = new ContentUseCases(repository);
+        var foundContent = await useCases.GetAsync(4378L);
+        Assert.Null(foundContent);
+    }
+    [Fact]
+    public async Task GetContents()
+    {
+        using ANTDbContext context = new();
+        var repository = new ContentRepository(context);
+        var useCases = new ContentUseCases(repository);
+        var Contents = await useCases.GetListAsync();
+        Assert.NotNull(Contents);
+        Assert.NotEmpty(Contents);
+        Assert.True(Contents.Count > 0);
+    }
+}
