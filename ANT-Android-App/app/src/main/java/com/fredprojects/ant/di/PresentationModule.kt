7e34089f629dff264bb08df2ca4da8aa191ceea9
FredNekrasov <frednekrasov49@gmail.com>
2024-10-19 11:46:23 +0300

    refactoring koin DI modules

--- ANT-Android-App/app/src/main/java/com/fredprojects/ant/di/PresentationModule.kt
+++ ANT-Android-App/app/src/main/java/com/fredprojects/ant/di/PresentationModule.kt
@@ -1,9 +1,16 @@
 package com.fredprojects.ant.di
 
+import com.fredprojects.ant.domain.repository.IArticleRepository
 import com.fredprojects.ant.presentation.screens.viewModels.ArticleVM
 import org.koin.core.module.dsl.viewModel
+import org.koin.core.qualifier.qualifier
 import org.koin.dsl.module
 
 val presentationModule = module {
-    viewModel { ArticleVM(get(), get()) }
+    viewModel(qualifier = qualifier<ArticleVM>()) {
+        ArticleVM(
+            get(qualifier<IArticleRepository>()),
+            get()
+        )
+    }
 }
