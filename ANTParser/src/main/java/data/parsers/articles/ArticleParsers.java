52fe1093a887832ced2371f28fb46fad863e099c
FredNekrasov <frednekrasov49@gmail.com>
2024-10-21 19:45:17 +0300

    removing unnecessary lines of code in records

--- ANTParser/src/main/java/data/parsers/articles/ArticleParsers.java
+++ ANTParser/src/main/java/data/parsers/articles/ArticleParsers.java
@@ -2,11 +2,8 @@ package data.parsers.articles;
 
 import data.parsers.articles.dynamicArticles.DAP;
 import data.parsers.articles.staticArticles.SAP;
-import jakarta.inject.Inject;
 
 public record ArticleParsers(
     DAP dynamicArticleParsers,
     SAP staticArticleParsers
-) {
-    @Inject public ArticleParsers {}
-}
+) {}
