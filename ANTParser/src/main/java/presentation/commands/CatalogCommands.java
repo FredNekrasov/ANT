8fa8cc86738a6422c5277dc8040789db879c4d34
FredNekrasov <frednekrasov49@gmail.com>
2024-10-22 19:15:36 +0300

    refactoring the `CatalogCommands.java` class

--- ANTParser/src/main/java/presentation/commands/CatalogCommands.java
+++ ANTParser/src/main/java/presentation/commands/CatalogCommands.java
@@ -5,33 +5,56 @@ import domain.useCases.CatalogUseCases;
 import jakarta.inject.Inject;
 
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class CatalogCommands {
     private final CatalogUseCases catalogUseCases;
     private final CatalogParser catalogParser;
-    private final PrintStream printer;
     @Inject
-    public CatalogCommands(CatalogUseCases catalogUseCases, CatalogParser catalogParser, PrintStream printStream) {
+    public CatalogCommands(CatalogUseCases catalogUseCases, CatalogParser catalogParser) {
         this.catalogUseCases = catalogUseCases;
         this.catalogParser = catalogParser;
-        this.printer = printStream;
     }
-    public void getParsedCatalogs() {
+    private void getParsedCatalogs(PrintStream printer) {
         catalogParser.parseCatalogs().forEach(printer::println);
     }
-    public void getRemoteCatalogs() {
+    private void getRemoteCatalogs(PrintStream printer) {
         catalogUseCases.getCatalogs().forEach(it -> printer.println(it.id() + " " + it.name()));
     }
-    public void upsertCatalog(String name) {
-        var status = catalogUseCases.upsertCatalog(name);
-        printer.println(status.name());
+    private void upsertCatalog() {
+        for (String name : catalogParser.parseCatalogs()) {
+            var status = catalogUseCases.upsertCatalog(name);
+            System.out.println(status.name());
+        }
     }
-    public void deleteCatalog(int id) {
+    private void deleteCatalog(int id) {
         var status = catalogUseCases.deleteCatalog(id);
-        printer.println(status.name());
+        System.out.println(status.name());
     }
-    public void getCatalogById(int id) {
+    private void getCatalogById(int id, PrintStream printer) {
         var catalog = catalogUseCases.getCatalogById(id);
         printer.println(catalog.id() + " " + catalog.name());
+    }
+    public void catalogMenu(Scanner scanner, PrintStream printer) {
+        var choice = -1;
+        while (choice != 0) {
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> getParsedCatalogs(printer);
+                case 2 -> getRemoteCatalogs(printer);
+                case 3 -> upsertCatalog();
+                case 4 -> {
+                    System.out.println("Enter catalog id: ");
+                    var id = scanner.nextInt();
+                    deleteCatalog(id);
+                }
+                case 5 -> {
+                    System.out.println("Enter catalog id: ");
+                    var id = scanner.nextInt();
+                    getCatalogById(id, printer);
+                }
+                default -> System.out.println("Invalid choice");
+            }
+        }
     }
 }
