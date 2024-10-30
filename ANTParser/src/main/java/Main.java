f6d54c686b887d1174d28faf8f003f4a8f9b8c3c
FredNekrasov <frednekrasov49@gmail.com>
2024-10-30 18:19:15 +0300

    sending data about youth club articles

--- ANTParser/src/main/java/Main.java
+++ ANTParser/src/main/java/Main.java
@@ -1,6 +1,5 @@
 import di.DaggerAppComponent;
-import domain.models.Article;
-import domain.models.Catalog;
+import domain.models.*;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -11,11 +10,23 @@ public class Main {
         var menu = DaggerAppComponent.create().getMenu();
         Scanner scanner = new Scanner(System.in);
         PrintStream printer = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        var scheduleParser = menu.parsers().articleParsers().staticArticleParsers().scheduleParser();
-		var article = new Article(new Catalog("Расписание Богослужений", 3L), scheduleParser.parseIf2Content().split("\n")[0], scheduleParser.parseIf2Content().split("\n")[1], "", 0L);
-        printer.println(article);
-        var txt = scanner.nextLine();
-        if (txt.isBlank()) menu.articleCommands().upsertArticle(article);
+        var parser = menu.parsers().articleParsers().dynamicArticleParsers().youthClub();
+        var choice = -1;
+        var id = 99L;
+        while (choice != 0) {
+            System.out.println("Enter the url");
+            var url = scanner.next();
+            var article = parser.getArticle(url, new Catalog("Молодежный клуб", 5L));
+            printer.println(article);
+            var content = parser.parseContent(url).split("\n");
+            for (int i = 0; i < content.length; i++) printer.println(i + "|" + content[i] + "|");
+            System.out.println("enter your choice");
+            choice = scanner.nextInt();
+            if (choice == 9) break;
+            menu.articleCommands().upsertArticle(article);
+            for (var c : content) menu.contentCommands().upsertContent(new Content(id, c, 0L));
+            id++;
+        }
         System.out.println("Hello, World!");
     }
 }
