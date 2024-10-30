472275fee172c1d9c79098362ab5760312c4315b
FredNekrasov <frednekrasov49@gmail.com>
2024-10-30 18:02:23 +0300

    sending schedule data

--- ANTParser/src/main/java/Main.java
+++ ANTParser/src/main/java/Main.java
@@ -1,6 +1,6 @@
 import di.DaggerAppComponent;
+import domain.models.Article;
 import domain.models.Catalog;
-import domain.models.Content;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -11,29 +11,11 @@ public class Main {
         var menu = DaggerAppComponent.create().getMenu();
         Scanner scanner = new Scanner(System.in);
         PrintStream printer = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-		var choice = -1;
-		var id = 2L;
-		var image = "";
-		while (choice != 0) {
-			System.out.println("Enter the url");
-			var url = scanner.next();
-			var article = menu.parsers().articleParsers().dynamicArticleParsers().parishLife().getArticle(url, new Catalog("Приходская жизнь", 2L));
-			printer.println(article);
-			var content = menu.parsers().articleParsers().dynamicArticleParsers().parishLife().parseContent(url).split("\n");
-			for (int i = 0; i < content.length; i ++) printer.println(i + "|" + content[i] + "|");
-			System.out.println("enter your choice");
-			choice = scanner.nextInt();
-			if (choice == 9) {
-				System.out.println("enter an image url");
-				image = scanner.next();
-			}
-			menu.articleCommands().upsertArticle(article);
-			if (image.isBlank()) {
-				for (var c : content) menu.contentCommands().upsertContent(new Content(id, c, 0L));
-			} else menu.contentCommands().upsertContent(new Content(id, image, 0L));
-			image = "";
-			id++;
-		}
+        var scheduleParser = menu.parsers().articleParsers().staticArticleParsers().scheduleParser();
+		var article = new Article(new Catalog("Расписание Богослужений", 3L), scheduleParser.parseIf2Content().split("\n")[0], scheduleParser.parseIf2Content().split("\n")[1], "", 0L);
+        printer.println(article);
+        var txt = scanner.nextLine();
+        if (txt.isBlank()) menu.articleCommands().upsertArticle(article);
         System.out.println("Hello, World!");
     }
 }
