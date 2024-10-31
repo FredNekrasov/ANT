1c92895aa94ad143be8112e005d6d9dc8b560b27
FredNekrasov <frednekrasov49@gmail.com>
2024-10-31 10:08:09 +0300

    sending out information about articles on the advice of the priest

--- ANTParser/src/main/java/Main.java
+++ ANTParser/src/main/java/Main.java
@@ -10,15 +10,30 @@ public class Main {
         var menu = DaggerAppComponent.create().getMenu();
         Scanner scanner = new Scanner(System.in);
         PrintStream printer = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        var parser = menu.parsers().articleParsers().staticArticleParsers().priesthoodParser();
-        var article = parser.getArticle(new Catalog("Священство", 6L));
-        printer.println(article);
-        var content = parser.parseContent();
-        System.out.println("|" + content + "|");
-        var choice = scanner.nextInt();
-        if (choice == 1) return;
-        menu.articleCommands().upsertArticle(article);
-        menu.contentCommands().upsertContent(new Content(113L, content, 0L));
+        var parser = menu.parsers().articleParsers().dynamicArticleParsers().advices();
+        var choice = -1;
+        var id = 114L;
+        var image = "";
+        while (choice != 0) {
+            System.out.println("Enter the url");
+            var url = scanner.next();
+            var article = parser.getArticle(url, new Catalog("Советы священника", 7L));
+            printer.println(article);
+            var content = parser.parseContent(url);
+            printer.println("|" + content + "|");
+            System.out.println("enter your choice");
+            choice = scanner.nextInt();
+            if (choice == 9) {
+                System.out.println("enter an image url");
+                image = scanner.next();
+            }
+            menu.articleCommands().upsertArticle(article);
+            if (image.isBlank()) {
+                menu.contentCommands().upsertContent(new Content(id, content, 0L));
+            } else menu.contentCommands().upsertContent(new Content(id, image, 0L));
+            id++;
+            image = "";
+        }
         System.out.println("Hello, World!");
     }
 }
