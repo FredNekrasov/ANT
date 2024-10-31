7879d129849edc21de8dc3bac23161c7af826b7a
FredNekrasov <frednekrasov49@gmail.com>
2024-10-31 11:32:08 +0300

    sending data about an article about contacts

--- ANTParser/src/main/java/Main.java
+++ ANTParser/src/main/java/Main.java
@@ -3,15 +3,22 @@ import domain.models.*;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         var menu = DaggerAppComponent.create().getMenu();
+        Scanner scanner = new Scanner(System.in);
         PrintStream printer = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        var parser = menu.parsers().articleParsers().staticArticleParsers().sacramentParser();
-        var article = parser.getArticle(new Catalog("Требы", 9L));
+        var parser = menu.parsers().articleParsers().staticArticleParsers().contactParser();
+        var article = parser.getArticle(new Catalog("Контакты", 10L));
+        var content = parser.parseContacts();
         printer.println(article);
+        for (var c : content) printer.println("|" + c + "|");
+        var isCorrect = scanner.next();
+        if (isCorrect.contentEquals("0")) return;
         menu.articleCommands().upsertArticle(article);
+        for (var c : content) menu.contentCommands().upsertContent(new Content(204L, c, 0L));
         System.out.println("Hello, World!");
     }
 }
