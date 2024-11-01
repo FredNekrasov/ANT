46127ecc317c69ca5967cd149332468b74e28293
FredNekrasov <frednekrasov49@gmail.com>
2024-11-01 20:20:45 +0300

    removing unnecessary classes

--- ANTParser/src/main/java/data/parsers/articles/ArticleParsers.java
+++
@@ -1,9 +0,0 @@
-package data.parsers.articles;
-
-import data.parsers.articles.dynamicArticles.DAP;
-import data.parsers.articles.staticArticles.SAP;
-
-public record ArticleParsers(
-    DAP dynamicArticleParsers,
-    SAP staticArticleParsers
-) {}
