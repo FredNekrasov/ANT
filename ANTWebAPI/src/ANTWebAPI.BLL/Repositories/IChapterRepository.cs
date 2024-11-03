b1311a8b244aec2b86503e4468289efe7f2d95a1
FredNekrasov <frednekrasov49@gmail.com>
2024-11-03 14:41:01 +0300

    adding a new method to the IChapterRepository.cs interface, which provides a list of chapters by catalog ID

--- ANTWebAPI/src/ANTWebAPI.BLL/Repositories/IChapterRepository.cs
+++ ANTWebAPI/src/ANTWebAPI.BLL/Repositories/IChapterRepository.cs
@@ -6,5 +6,6 @@ public interface IChapterRepository
 {
     Task<List<Chapter>> GetListAsync();
     Task<List<Chapter>> GetPagedListAsync(int pageNumber, int pageSize);
+    Task<List<Chapter>> GetListByCatalogAsync(int catalogId);
     Task<int> GetTotalCountAsync();
 }
