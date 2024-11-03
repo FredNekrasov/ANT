b291db31eac095ccc49f80cd1b6f5aa2250471ea
FredNekrasov <frednekrasov49@gmail.com>
2024-11-03 14:45:16 +0300

    creating a response class for paged data

---
+++ ANTWebAPI/src/ANTWebAPI.PL/DTOs/PagedResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ANTWebAPI.PL.DTOs;
+
+public class PagedResponse<T>(int pageNumber, int pageSize, int totalRecords, ICollection<T> data)
+{
+    public int PageNumber { get; set; } = pageNumber;
+    public int PageSize { get; set; } = pageSize;
+    public int TotalRecords { get; set; } = totalRecords;
+    public ICollection<T> Data { get; set; } = data;
+}
