0f66968cece68d1786ea940441c59635acdb7367
FredNekrasov <frednekrasov49@gmail.com>
2024-11-08 22:05:55 +0300

    adding comments to server mappers

--- ANTWebAPI/src/ANTWebAPI.PL/Mappers/ArticleMapper.cs
+++ ANTWebAPI/src/ANTWebAPI.PL/Mappers/ArticleMapper.cs
@@ -3,8 +3,18 @@ using ANTWebAPI.PL.DTOs;
 
 namespace ANTWebAPI.PL.mappers;
 
+/*
+ * Article mapper is used for extension methods to map ArticleDTO in the Article model and vice versa
+ */
 public static class ArticleMapper
 {
+    /*
+     * ToDto is extension method which returns ArticleDTO from Article
+     * 
+     * @param article
+     * 
+     * @return ArticleDTO
+     */
     public static ArticleDTO ToDto(this Article article) => new()
     {
         Id = article.Id,
@@ -13,7 +23,13 @@ public static class ArticleMapper
         Description = article.Description,
         DateOrBanner = article.DateOrBanner
     };
-
+    /*
+     * ToModel is extension method which returns Article from ArticleDTO
+     * 
+     * @param articleDto
+     * 
+     * @return Article
+     */
     public static Article ToModel(this ArticleDTO articleDto) => new()
     {
         Id = articleDto.Id,
--- ANTWebAPI/src/ANTWebAPI.PL/Mappers/ChapterMapper.cs
+++ ANTWebAPI/src/ANTWebAPI.PL/Mappers/ChapterMapper.cs
@@ -3,8 +3,18 @@ using ANTWebAPI.PL.DTOs;
 
 namespace ANTWebAPI.PL.mappers;
 
+/*
+ * ChapterMapper is used for extension methods to map Chapter in the ChapterDTO
+ */
 public static class ChapterMapper
 {
+    /*
+     * ToDto is extension method that maps Chapter to ChapterDTO
+     * 
+     * @param chapter Chapter
+     * 
+     * @return ChapterDTO
+     */
     public static ChapterDTO ToDto(this Chapter chapter) => new()
     {
         Id = chapter.Id,
