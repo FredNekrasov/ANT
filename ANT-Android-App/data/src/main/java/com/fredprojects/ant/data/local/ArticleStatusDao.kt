ce91100c3f78f54b53dd3fa88908d08f69db075c
FredNekrasov <frednekrasov49@gmail.com>
2024-11-10 11:33:57 +0300

    refactoring the ArticleStatusDao by renaming function names

--- ANT-Android-App/data/src/main/java/com/fredprojects/ant/data/local/ArticleStatusDao.kt
+++ ANT-Android-App/data/src/main/java/com/fredprojects/ant/data/local/ArticleStatusDao.kt
@@ -18,17 +18,17 @@ class ArticleStatusDao(
         db.articleStatusQueries.getAllArticleStatusData().executeAsList()
     }
     /**
-     * Upsert article in the database
+     * Upsert article status in the database
      * @param articleStatus is an ArticleStatus class that will be inserted in the database if it doesn't exist or updated otherwise
      */
-    suspend fun upsertArticle(articleStatus: ArticleStatusEntity) = withContext(Dispatchers.IO) {
+    suspend fun upsertArticleStatus(articleStatus: ArticleStatusEntity) = withContext(Dispatchers.IO) {
         db.articleStatusQueries.upsertArticleStatus(articleStatus)
     }
     /**
-     * Delete article from the database
+     * Delete article status from the database
      * @param id is an ID of the article that will be deleted
      */
-    suspend fun deleteArticle(id: Long) = withContext(Dispatchers.IO) {
+    suspend fun deleteArticleStatus(id: Long) = withContext(Dispatchers.IO) {
         db.articleStatusQueries.deleteArticleStatus(id)
     }
 }
