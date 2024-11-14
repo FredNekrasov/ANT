ce12442117462a73de3a22223ced4dd548661d5a
FredNekrasov <frednekrasov49@gmail.com>
2024-11-14 13:05:34 +0300

    removing an unnecessary function from the ArticleVM

--- ANT-Android-App/presentation/src/main/java/com/fredprojects/ant/presentation/screens/viewModels/ArticleVM.kt
+++ ANT-Android-App/presentation/src/main/java/com/fredprojects/ant/presentation/screens/viewModels/ArticleVM.kt
@@ -1,8 +1,5 @@
 package com.fredprojects.ant.presentation.screens.viewModels
 
-import android.app.Application
-import android.content.Intent
-import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fredprojects.ant.domain.repository.IArticleRepository
@@ -12,16 +9,15 @@ import kotlinx.coroutines.launch
 
 class ArticleVM(
     private val repository: IArticleRepository,
-    private val context: Application
 ) : ViewModel() {
     private val articlesMSF = MutableStateFlow(ArticleState())
     val articlesSF = articlesMSF.asStateFlow()
-    fun getArticles(catalogId: Int, pageNumber: Int) {
+    fun getArticles(catalogId: Int, pageNumber: Int = 25) {
         viewModelScope.launch {
             repository.getList(catalogId, pageNumber).flowOn(Dispatchers.IO).collectLatest {
                 articlesMSF.emit(articlesSF.value.copy(list = it.list, status = it.status))
             }
         }
     }
-    fun openApp(url: String) = context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+    init { getArticles(1) }
 }
