aafe79015bce2873a351a11b392a5f8c30a0d999
FredNekrasov <frednekrasov49@gmail.com>
2024-11-18 19:48:16 +0300

    refactoring the ANTNavHost function

--- ANT-Android-App/app/src/main/java/com/fredprojects/ant/navigation/ANTNavHost.kt
+++ ANT-Android-App/app/src/main/java/com/fredprojects/ant/navigation/ANTNavHost.kt
@@ -3,29 +3,32 @@ package com.fredprojects.ant.navigation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.fredprojects.ant.presentation.core.ANTStrings
-import com.fredprojects.ant.presentation.core.Action
+import com.fredprojects.ant.presentation.core.*
 import com.fredprojects.ant.presentation.screens.*
 import com.fredprojects.ant.presentation.screens.onePages.*
 import com.fredprojects.ant.presentation.screens.viewModels.ArticleVM
+import kotlinx.coroutines.flow.collectLatest
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.qualifier.qualifier
 
 @Composable
 fun ANTNavHost(
     controller: NavHostController,
-    articleVM: ArticleVM,
-    closeDialog: Action,
+    openApp: (String) -> Unit,
     modifier: Modifier = Modifier,
+    articleVM: ArticleVM = koinViewModel<ArticleVM>(qualifier<ArticleVM>()),
     navItems: SnapshotStateList<String> = ANTStrings.screens
 ) {
     val state = articleVM.articlesSF.collectAsState().value
+    val context = LocalContext.current
     NavHost(navController = controller, startDestination = navItems[0]) {
         composable(navItems[0]) { MainScreen(state) }
         composable(navItems[1]) { ParishLife(state) }
@@ -36,8 +39,13 @@ fun ANTNavHost(
         composable(navItems[6]) { Advices(state) }
         composable(navItems[7]) { History(state) }
         composable(navItems[8]) { Sacraments(state) }
-        composable(navItems[9]) { Contacts(state, closeDialog, articleVM::openApp) }
+        composable(navItems[9]) { Contacts(state, openApp) }
         composable(navItems[10]) { Box(modifier) { LinearProgressIndicator(Modifier.align(Alignment.Center)) } }
         composable(navItems[11]) { Volunteerism(state) }
+    }
+    LaunchedEffect(state.status) {
+        articleVM.articlesSF.collectLatest {
+            if(it.status.isError()) context.displayMessage(it.status.getMessage())
+        }
     }
 }
