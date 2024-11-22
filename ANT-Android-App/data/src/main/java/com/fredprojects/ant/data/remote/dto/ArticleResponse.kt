9c911bc4049b9610ae3212e0c71285c2ec832c17
FredNekrasov <frednekrasov49@gmail.com>
2024-11-22 19:49:35 +0300

    adding a serializable annotation to the ArticleResponse class

--- ANT-Android-App/data/src/main/java/com/fredprojects/ant/data/remote/dto/ArticleResponse.kt
+++ ANT-Android-App/data/src/main/java/com/fredprojects/ant/data/remote/dto/ArticleResponse.kt
@@ -1,5 +1,7 @@
 package com.fredprojects.ant.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
 /**
  * ArticleResponse would be received from the server
  * @param pageNumber is a number of the page
@@ -7,6 +9,7 @@ package com.fredprojects.ant.data.remote.dto
  * @param totalRecords is a total number of records
  * @param data is a list of articles
  */
+@Serializable
 data class ArticleResponse(
     val pageNumber: Int,
     val pageSize: Int,
