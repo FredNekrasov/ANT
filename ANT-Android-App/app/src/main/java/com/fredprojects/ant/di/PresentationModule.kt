4e93a962f9085e7b0c15d1556251dc2ef95e305b
FredNekrasov <frednekrasov49@gmail.com>
2024-12-18 14:20:55 +0300

    creating new viewModels for screens that used pagination

--- ANT-Android-App/app/src/main/java/com/fredprojects/ant/di/PresentationModule.kt
+++ ANT-Android-App/app/src/main/java/com/fredprojects/ant/di/PresentationModule.kt
@@ -1,13 +1,28 @@
 package com.fredprojects.ant.di
 
 import com.fredprojects.ant.domain.repository.IArticleRepository
-import com.fredprojects.ant.presentation.screens.viewModels.ArticleVM
+import com.fredprojects.ant.presentation.screens.viewModels.*
 import org.koin.core.module.dsl.viewModel
 import org.koin.core.qualifier.qualifier
 import org.koin.dsl.module
 
 val presentationModule get() = module {
-    viewModel(qualifier = qualifier<ArticleVM>()) {
-        ArticleVM(get(qualifier<IArticleRepository>()))
+    viewModel(qualifier<MainVM>()) {
+        MainVM(get(qualifier<IArticleRepository>()))
+    }
+    viewModel(qualifier<ParishLifeVM>()) {
+        ParishLifeVM(get(qualifier<IArticleRepository>()))
+    }
+    viewModel(qualifier<YouthClubVM>()) {
+        YouthClubVM(get(qualifier<IArticleRepository>()))
+    }
+    viewModel(qualifier<AdvicesVM>()) {
+        AdvicesVM(get(qualifier<IArticleRepository>()))
+    }
+    viewModel(qualifier<HistoryVM>()) {
+        HistoryVM(get(qualifier<IArticleRepository>()))
+    }
+    viewModel(qualifier<StoriesVM>()) {
+        StoriesVM(get(qualifier<IArticleRepository>()))
     }
 }
