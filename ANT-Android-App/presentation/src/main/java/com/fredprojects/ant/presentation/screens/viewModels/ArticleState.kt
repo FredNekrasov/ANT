becd199365c9abf8880220188cd8da3455296782
FredNekrasov <frednekrasov49@gmail.com>
2024-12-18 14:20:34 +0300

    creating new state class for paged data

--- ANT-Android-App/presentation/src/main/java/com/fredprojects/ant/presentation/screens/viewModels/ArticleState.kt
+++ ANT-Android-App/presentation/src/main/java/com/fredprojects/ant/presentation/screens/viewModels/ArticleState.kt
@@ -5,7 +5,13 @@ import com.fredprojects.ant.domain.models.Article
 import com.fredprojects.ant.domain.utils.ConnectionStatus
 
 @Stable
-data class ArticleState(
+data class MainArticleState(
     val list: List<Article> = emptyList(),
     val status: ConnectionStatus = ConnectionStatus.LOADING
+)
+@Stable
+data class PagedArticleState(
+    val map: Map<Int, List<Article>> = emptyMap(),
+    val status: ConnectionStatus = ConnectionStatus.LOADING,
+    val hasNextData: Boolean = false
 )
