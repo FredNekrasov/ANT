6a3b54629a1b532358c56b354b9792f985941be8
FredNekrasov <frednekrasov49@gmail.com>
2025-01-25 18:57:31 +0300

    refactoring the **parser** (HttpRequests, NetworkProvider)

--- ANTParser/src/main/java/data/remote/api/HttpRequests.java
+++ ANTParser/src/main/java/data/remote/api/HttpRequests.java
@@ -6,24 +6,26 @@ import java.net.URI;
 import java.net.http.HttpRequest;
 
 public final class HttpRequests {
-    private final HttpRequest.Builder request = HttpRequest.newBuilder();
-    @Inject public HttpRequests() {}
+    private final HttpRequest.Builder requestBuilder;
+    @Inject public HttpRequests(HttpRequest.Builder requestBuilder) {
+        this.requestBuilder = requestBuilder;
+    }
     public HttpRequest get(String url) {
-        return request.uri(URI.create(url)).GET().build();
+        return requestBuilder.uri(URI.create(url)).GET().build();
     }
     public HttpRequest post(String url, String body) {
-        return request.uri(URI.create(url))
+        return requestBuilder.uri(URI.create(url))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(body))
                 .build();
     }
     public HttpRequest put(String url, String body) {
-        return request.uri(URI.create(url))
+        return requestBuilder.uri(URI.create(url))
                 .header("Content-Type", "application/json")
                 .PUT(HttpRequest.BodyPublishers.ofString(body))
                 .build();
     }
     public HttpRequest delete(String url) {
-        return request.uri(URI.create(url)).DELETE().build();
+        return requestBuilder.uri(URI.create(url)).DELETE().build();
     }
 }
