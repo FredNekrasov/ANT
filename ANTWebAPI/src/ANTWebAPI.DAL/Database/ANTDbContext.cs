98eded03a7952b76049de802bca49800b95a5eaa
FredNekrasov <frednekrasov49@gmail.com>
2025-01-29 22:33:53 +0300

    removing unnecessary comments and migrating from sql server to postgresql

--- ANTWebAPI/src/ANTWebAPI.DAL/Database/ANTDbContext.cs
+++ ANTWebAPI/src/ANTWebAPI.DAL/Database/ANTDbContext.cs
@@ -4,28 +4,14 @@ using System.Reflection;
 
 namespace ANTWebAPI.DAL.Database;
 
-/*
- * ANTDbContext is a database context class that is used to access the ANT database.
- */
 public class ANTDbContext : DbContext
 {
-    /*
-     * OnConfiguring is used to configure the database context.
-     * 
-     * The connection string is used to connect to the ANT database.
-     * The integrated security is used to authenticate the user.
-     * The initial catalog is used to specify the database.
-     */
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ANT;Integrated Security=true;");
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=ANT;");
     }
-    /*
-     * OnModelCreating is used to configure the database model.
-     * 
-     * The Assembly.GetExecutingAssembly() method is used to get the assembly that contains the configurations.
-     */
+    // The `Assembly.GetExecutingAssembly()` method is used to get the assembly that contains entity configurations.
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
