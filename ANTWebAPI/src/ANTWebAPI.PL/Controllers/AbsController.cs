5c8377a62b1d069e0a2edd18b1f8250f365db7b4
FredNekrasov <frednekrasov49@gmail.com>
2025-01-30 20:24:45 +0300

    editing comments from the AbsController.cs class

--- ANTWebAPI/src/ANTWebAPI.PL/Controllers/AbsController.cs
+++ ANTWebAPI/src/ANTWebAPI.PL/Controllers/AbsController.cs
@@ -2,48 +2,39 @@
 
 namespace ANTWebAPI.PL.Controllers;
 
-/*
- * AbsController is an abstract class that provides common methods for all controllers
- */
 public abstract class AbsController<D> : ControllerBase
 {
-    /*
-     * GetListAsync is an abstract method that returns an ActionResult of ICollection<D>
-     * 
-     * @return an ActionResult of ICollection<D>
-     */
+    /**
+     * GetListAsync method returns all records from database
+     *
+     * if records are not found, NoContent()/204 is returned else Ok()/200 is returned
+    **/
     public abstract Task<ActionResult<ICollection<D>>> GetListAsync();
-    /*
-     * PutRecordAsync is an abstract method that returns an IActionResult
-     * 
-     * @param id the id of the record to update
-     * @param dto the dto of the record to update
-     * 
-     * @return an IActionResult
-     */
     public abstract Task<IActionResult> PutRecordAsync(long id, D dto);
-    /*
-     * PostRecordAsync is an abstract method that returns an IActionResult
-     * 
-     * @param dto the dto of the record to insert
-     * 
-     * @return an IActionResult
-     */
+    /**
+     * PostRecordAsync method inserts record into database
+     *
+     * @param D dto is the record that should be inserted
+     *
+     * if record is inserted successfully, Ok()/200 is returned else BadRequest()/400 is returned
+    **/
     public abstract Task<IActionResult> PostRecordAsync(D dto);
-    /*
-     * DeleteRecordAsync is an abstract method that returns an IActionResult
-     * 
-     * @param id the id of the record to delete
-     * 
-     * @return an IActionResult
-     */
+    /**
+     * DeleteRecordAsync method deletes record from database by id
+     *
+     * @param long id - id of the record that should be deleted
+     *
+     * if record is not found, NotFound()/404 is returned
+     * if record is deleted successfully, Ok()/200 is returned
+     * if record is used as a foreign key in other entity records, BadRequest()/400 is returned
+    **/
     public abstract Task<IActionResult> DeleteRecordAsync(long id);
-    /*
-     * GetRecordAsync is an abstract method that returns an ActionResult of D
-     * 
-     * @param id the id of the record to get
-     * 
-     * @return an ActionResult of D
-     */
+    /**
+     * GetRecordAsync method returns record from database by id
+     *
+     * @param long id - id of the record that should be returned
+     *
+     * if record is not found, NotFound()/404 is returned else Ok()/200 is returned
+    **/
     public abstract Task<ActionResult<D>> GetRecordAsync(long id);
 }
