6fabbea520bb7739044d6303847b511fb9c0309e
FredNekrasov <frednekrasov49@gmail.com>
2025-01-30 20:25:35 +0300

    removing unnecessary comments from PagedResponse.cs and VerifyExtensions.cs classes

--- ANTWebAPI/src/ANTWebAPI.PL/DTOs/PagedResponse.cs
+++ ANTWebAPI/src/ANTWebAPI.PL/DTOs/PagedResponse.cs
@@ -1,8 +1,5 @@
 ﻿namespace ANTWebAPI.PL.DTOs;
 
-/*
- * Paged Response DTO is used to send the paged response to the client
- */
 public class PagedResponse<T>(int pageNumber, int pageSize, int totalRecords, ICollection<T> data)
 {
     public int PageNumber { get; set; } = pageNumber;
